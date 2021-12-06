86e4ec31970294043face8f5d07e0fe732b5368e
Michael Iringan <miringan@uw.edu>
2021-12-05 22:31:12 -0800

    The following changes have been made to the README.md: set the Modified field to 12/5/21, added text to line 8, added a link to a screenshot on line 10. The following changes have been made to the script.js file: created vars saveButtonEl9 thru saveButtonEl17 to capture save buttons for each timeslot save button element, created vars timeSlotTextEl9 thru timeSlotTextEl17 to capture timeSlotText values for each time slot, created click events for each time slot save button to save user input to local storage. Added screenshot.png to directory.

--- script.js
+++ script.js
@@ -30,7 +30,6 @@ for (var i = 9; i < 18; i++){
 
     // Assign attributes to the divs
     timeSlotEl.setAttribute('id', 'hour ' + i);
-    timeSlotEl.setAttribute('data-time', i);
 
     // Establishing current hour variable for comparison
     var currentHour = moment().format('h')
@@ -57,16 +56,19 @@ for (var i = 9; i < 18; i++){
     var timeSlotTextEl = document.createElement('textarea');
 
     // Assign attributes to the textarea
-    timeSlotTextEl.setAttribute('id', 'Hour ' + i + ' response');
+    timeSlotTextEl.setAttribute('id', 'Hour-' + i + '-Response');
     timeSlotTextEl.setAttribute('class', 'textarea description');
 
     // Create a save button for each hour of the day
     var saveButtonEl = document.createElement('button');
 
     // Assign attributes to the save button
-    saveButtonEl.setAttribute('class', 'saveBtn');
+    saveButtonEl.setAttribute('class', 'saveBtn col-md-1');
+    saveButtonEl.setAttribute('id', 'Button-' + i);
     saveButtonEl.innerHTML = '💾';
 
+    // Create i element to go within each button
+    var saveButtonI = document.createElement('i');
 
     // Add the div to the containerEl
     containerEl.appendChild(timeSlotEl);
@@ -74,6 +76,69 @@ for (var i = 9; i < 18; i++){
     // Add the text areas to the time slot divs
     timeSlotEl.appendChild(timeSlotTextEl);
 
-    // Add the save button to the time slot dic
+    // Add the save button to the time slot div
     timeSlotEl.appendChild(saveButtonEl);
+
+    // Load textarea content with local storage information
+    timeSlotTextEl.value = localStorage.getItem("Hour " + i + " response");
 }
+
+
+// Capture all save buttons
+var saveButtonEl9 = $('#Button-9');
+var saveButtonEl10 = $('#Button-10');
+var saveButtonEl11 = $('#Button-11');
+var saveButtonEl12 = $('#Button-12');
+var saveButtonEl13 = $('#Button-13');
+var saveButtonEl14 = $('#Button-14');
+var saveButtonEl15 = $('#Button-15');
+var saveButtonEl16 = $('#Button-16');
+var saveButtonEl17 = $('#Button-17');
+
+// Capture text area descriptions
+var timeSlotTextEl9 = $('#Hour-9-Response');
+var timeSlotTextEl10 = $('#Hour-10-Response');
+var timeSlotTextEl11 = $('#Hour-11-Response');
+var timeSlotTextEl12 = $('#Hour-12-Response');
+var timeSlotTextEl13 = $('#Hour-13-Response');
+var timeSlotTextEl14 = $('#Hour-14-Response');
+var timeSlotTextEl15 = $('#Hour-15-Response');
+var timeSlotTextEl16 = $('#Hour-16-Response');
+var timeSlotTextEl17 = $('#Hour-17-Response');
+
+// Saving timeSlotText values (user input) to local storage by hour.
+saveButtonEl9.on('click', function() {
+    localStorage.setItem("Hour 9 response", timeSlotTextEl9.val());
+})
+
+saveButtonEl10.on('click', function() {
+    localStorage.setItem("Hour 10 response", timeSlotTextEl10.val());
+})
+
+saveButtonEl11.on('click', function() {
+    localStorage.setItem("Hour 11 response", timeSlotTextEl11.val());
+})
+
+saveButtonEl12.on('click', function() {
+    localStorage.setItem("Hour 12 response", timeSlotTextEl12.val());
+})
+
+saveButtonEl13.on('click', function() {
+    localStorage.setItem("Hour 13 response", timeSlotTextEl13.val());
+})
+
+saveButtonEl14.on('click', function() {
+    localStorage.setItem("Hour 14 response", timeSlotTextEl14.val());
+})
+
+saveButtonEl15.on('click', function() {
+    localStorage.setItem("Hour 15 response", timeSlotTextEl15.val());
+})
+
+saveButtonEl16.on('click', function() {
+    localStorage.setItem("Hour 16 response", timeSlotTextEl16.val());
+})
+
+saveButtonEl17.on('click', function() {
+    localStorage.setItem("Hour 17 response", timeSlotTextEl17.val());
+})
